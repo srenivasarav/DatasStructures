a84c9a238968071d8ff5d0a857cadfb8c29b7b14
Srinivasa Rao B <srenivas35@gmail.com>
2020-01-26 18:15:34 +0530

    Staging Files and Arithmetic

---
+++ aritmetic.py
@@ -0,0 +1,9 @@
+def add(x,y):
+[Z    pass
+def substract(x,y):
+    pass
+def multiply(x,y)
+    pass
+git divide(x,y):[A[A[C:[B[B[B[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Ddef[C[C[C[C[C[C[C[C[C[C[C[C[C
+    pass
+def multiply(x,y):
